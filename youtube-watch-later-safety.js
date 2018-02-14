e7d5c7cc6c936cc0b86baa0a900045c86a07fd48
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2018-02-14 08:38:06 -0800

    fix match URL, jquery URL

--- youtube-watch-later-safety.js
+++ youtube-watch-later-safety.js
@@ -3,10 +3,11 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Remove watchlist overlays on the Watch Later page. Never lose a video again!
-// @author       chaimleib
+// @author       Chaim Leib Halbert
 // @match        https://www.youtube.com/watch?*list=WL*
+// @match        https://www.youtube.com/watch?*list=WL
 // @grant        none
-// @require http://code.jquery.com/jquery-latest.slim.min.js
+// @require http://code.jquery.com/jquery-latest.min.js
 // @require https://raw.githubusercontent.com/uzairfarooq/arrive/master/minified/arrive.min.js
 // @updateURL   https://raw.githubusercontent.com/chaimleib/youtube-watch-later-safety/master/youtube-watch-later-safety.js
 // @downloadURL   https://raw.githubusercontent.com/chaimleib/youtube-watch-later-safety/master/youtube-watch-later-safety.js
@@ -21,3 +22,4 @@
         }
     );
 })();
+
