3eb2cf34ef8d40dec258fdb0d3fbc207c9c78e7b
Chaim Halbert <chalbert@evernote.com>
2018-02-14 18:55:46 -0800

    fix homepage

--- youtube-watch-later-safety.js
+++ youtube-watch-later-safety.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         YouTube Watch Later safety
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    https://github.com/chaimleib/youtube-watch-later-safety/
+// @version      0.2
 // @description  Remove watchlist overlays on the Watch Later page. Never lose a video again!
 // @author       Chaim Leib Halbert
 // @match        https://www.youtube.com/watch?*list=WL*
 // @match        https://www.youtube.com/watch?*list=WL
 // @grant        none
-// @require http://code.jquery.com/jquery-latest.min.js
-// @require https://raw.githubusercontent.com/uzairfarooq/arrive/master/minified/arrive.min.js
-// @updateURL   https://raw.githubusercontent.com/chaimleib/youtube-watch-later-safety/master/youtube-watch-later-safety.js
-// @downloadURL   https://raw.githubusercontent.com/chaimleib/youtube-watch-later-safety/master/youtube-watch-later-safety.js
+// @require      http://code.jquery.com/jquery-latest.min.js
+// @require      https://raw.githubusercontent.com/uzairfarooq/arrive/master/minified/arrive.min.js
+// @updateURL    https://raw.githubusercontent.com/chaimleib/youtube-watch-later-safety/master/youtube-watch-later-safety.js
+// @downloadURL  https://raw.githubusercontent.com/chaimleib/youtube-watch-later-safety/master/youtube-watch-later-safety.js
 // ==/UserScript==
 
 (function() {
